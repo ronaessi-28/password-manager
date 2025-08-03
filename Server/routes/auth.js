6b36a05ec8b32d49cb735cdbf55319df199da2f8
Viney Chhillar <ronaessi28chhillar@gmail.com>
2025-08-03 22:01:29 +0530

    Update auth.js

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const router = express.Router();
+const router = express.Router(); 
 
 router.post('/signup', async (req, res) => {
     const { email, password } = req.body;
