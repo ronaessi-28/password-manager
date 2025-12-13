74d2ba07f687e3140a8b416a34baed56e47cc0eb
Viney Chhillar <ronaessi28chhillar@gmail.com>
2025-12-13 20:29:53 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose') ;
 const dotenv = require('dotenv');    
 const cors = require('cors');  
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/auth'); 
 const passwordRoutes = require('./routes/passwords'); 
 
 dotenv.config(); 
